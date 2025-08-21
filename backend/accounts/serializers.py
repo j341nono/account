9e766e86b8f81af930d93b34e7436b70233373f8
j341nono <kanakana.nono1224@gmail.com>
2025-08-21 21:22:51 +0900

    feat: Implement account authentication

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
 from .models import User
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('user_id', 'password', 'nickname')
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import RegisterSerializer #,LoginSerializer,UserUpdateSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+from .models import  User #, AccessToken
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+
+
+class RegisterView(APIView):
+    @staticmethod
+    def post(request, *args, **kwargs):
+        print(request.data)
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            if serializer.validated_data['password'] != request.data['password_confirmation']:
+                return Response({'error': 2}, status=HTTP_400_BAD_REQUEST)
+
+            if User.objects.filter(user_id=serializer.validated_data['user_id']).exists():
+                return Response({'error': 3}, status=HTTP_400_BAD_REQUEST)
+
+            try:
+                serializer.save()
+            except:
+                # データベースエラー
+                return Response({'error': 11}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
